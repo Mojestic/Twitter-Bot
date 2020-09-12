37ced38c851015959d7018bd0b6ff380aa43b0ad
Mojestic <68197895+Mojestic@users.noreply.github.com>
2020-09-12 23:10:05 +0530

    Add files via upload

---
+++ Runbot.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 23:07:40 2020
+
+@author: Sourabh
+"""
+
+import twitbot
+
+
+def main():
+    api_keys = ('',
+                '')
+    access_tokens = ('',
+                     '')
+    
+    
+    
